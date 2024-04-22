424c16a3ef8a6dff0fe82d0ce8735b085c59201a
samen22 <samenjoy27@gmail.com>
2024-04-23 00:22:22 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div className='p-10 flex flex-col'>
+      <h1 className='text-center text-4xl font-semibold'>
+        Framer motion components
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
